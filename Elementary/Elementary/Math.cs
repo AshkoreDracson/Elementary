7e9765391f828c8de9d619d675c24425f66aa3da
AshkoreDracson <AshkoreD@gmail.com>
2017-01-01 04:10:15 +0100

    Added Clamp in Math

--- Elementary/Elementary/Math.cs
+++ Elementary/Elementary/Math.cs
@@ -11,6 +11,184 @@ namespace Elementary
             rnd = new Random();
         }
 
+        public static sbyte Clamp(sbyte value, sbyte min, sbyte max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static short Clamp(short value, short min, short max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static long Clamp(long value, long min, long max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static byte Clamp(byte value, byte min, byte max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static ushort Clamp(ushort value, ushort min, ushort max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static uint Clamp(uint value, uint min, uint max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static ulong Clamp(ulong value, ulong min, ulong max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static float Clamp(float value, float min, float max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static double Clamp(double value, double min, double max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+        public static decimal Clamp(decimal value, decimal min, decimal max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
+        }
+
+        public static sbyte Clamp01(sbyte value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static short Clamp01(short value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static int Clamp01(int value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static long Clamp01(long value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static byte Clamp01(byte value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static ushort Clamp01(ushort value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static uint Clamp01(uint value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static ulong Clamp01(ulong value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static float Clamp01(float value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static double Clamp01(double value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+        public static decimal Clamp01(decimal value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 1)
+                return 1;
+            return value;
+        }
+
         public static int Random()
         {
             return rnd.Next();
@@ -22,6 +200,20 @@ namespace Elementary
         public static int Random(int min, int max)
         {
             return rnd.Next(min, max);
+        }
+
+        public static float RandomFloat()
+        {
+            return (float)rnd.NextDouble();
+        }
+        public static double RandomDouble()
+        {
+            return rnd.NextDouble();
+        }
+
+        public static float Remap(float value, float inputMin, float inputMax, float outputMin, float outputMax)
+        {
+            return value;
         }
     }
 }
