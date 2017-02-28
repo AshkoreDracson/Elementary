e78eccd9adecac2cbb8d38b2212a8a634c9f383f
AshkoreDracson <AshkoreD@gmail.com>
2017-02-28 23:25:54 +0100

    Added IEnumerable extensions Added Combinations(string, int) to generate an enumerable list of string of all possible combinations of all the chars in the specified string for a specified length

---
+++ Elementary/Elementary/IEnumerableExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Elementary
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<string> Combinations(string s, int length)
+        {
+            if (length < 1)
+                throw new Exception("The length cannot be inferior to 1");
+
+            var q = s.Select(x => x.ToString());
+            for (int i = 0; i < length - 1; i++)
+                q = q.SelectMany(x => s, (x, y) => x + y);
+
+            return q;
+        }
+    }
+}
