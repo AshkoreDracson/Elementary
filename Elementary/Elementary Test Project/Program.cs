2c1d652a19db0df6ef23ddec0d1ff3f68535cd42
AshkoreDracson <AshkoreD@gmail.com>
2017-03-02 02:00:24 +0100

    Updated Test Project

--- Elementary/Elementary Test Project/Program.cs
+++ Elementary/Elementary Test Project/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Elementary;
 using System;
+using System.Diagnostics;
 using System.Threading;
 namespace Elementary_Test_Project
 {
@@ -8,11 +9,18 @@ namespace Elementary_Test_Project
         static void Main(string[] args)
         {
             Logman.LogOutput = LogOutput.Console | LogOutput.ImmediateWindow | LogOutput.File;
-            Logman.FileOutputPath = "./log.txt";
+            Logman.FileOutputPath = ".\\log.txt";
             Logman.Log("Test");
             Thread.Sleep(2000);
             Logman.Log("This is another test");
+            Thread.Sleep(1000);
+            Logman.Log("This is an information message", LogLevel.Information);
+            Thread.Sleep(1000);
+            Logman.Log("This is a warning message", LogLevel.Warning);
+            Thread.Sleep(1000);
+            Logman.Log("This is an error message", LogLevel.Error);
             Console.ReadKey();
+            Process.Start(".\\log.txt");
         }
     }
 }
