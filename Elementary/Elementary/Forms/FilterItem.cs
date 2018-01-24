ce9d4df8bad43df06eb09762a83270a8b9657fb8
AshkoreDracson <AshkoreD@gmail.com>
2018-01-24 04:33:52 +0100

    Added documentation to FilterItem

--- Elementary/Elementary/Forms/FilterItem.cs
+++ Elementary/Elementary/Forms/FilterItem.cs
@@ -2,7 +2,13 @@
 {
     public struct FilterItem
     {
+        /// <summary>
+        /// The text of this <see cref="FilterItem"/>
+        /// </summary>
         public string Text { get; }
+        /// <summary>
+        /// The extensions of this <see cref="FilterItem"/>
+        /// </summary>
         public string[] Extensions { get; }
 
         public FilterItem(string text, params string[] extensions)
